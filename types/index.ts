2e086193c4e9d426314a1402cdc02c087ab4da81
Pawan <pawantamada8@gmail.com>
2023-06-13 13:17:24 +0530

    fixed typo

--- types/index.ts
+++ types/index.ts
@@ -64,5 +64,5 @@ export interface ShowMoreProps {
 
 export interface SearchManufacturersProps {
   manufacturer: string;
-  setManuFacturer: (manufacturer: string) => void;
+  setManufacturer: (manufacturer: string) => void;
 }
